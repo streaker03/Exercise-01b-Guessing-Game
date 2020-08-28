55c264db067397d0a4c17f50a3b2762c7c7aaa37
streaker03 <andrewgostomelsky@gmail.com>
2020-08-27 22:45:45 -0400

    finished

--- main2.py
+++ main2.py
@@ -1,2 +1,15 @@
 import sys, random
 assert sys.version_info >= (3,8), "This script requires at least Python 3.8"
+check = True
+while check:
+    number = 5
+    guess = input("Guess a number from 1 to 10: ")
+    if guess.isdigit():
+        if 1 <= int(guess) <= 10:
+            check = False
+
+if guess == number:
+    print("Great job! You got it!")
+else:
+    print("Sorry, better luck next time.")
+    print("The number was " + str(number))
